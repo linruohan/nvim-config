6096978a7117a6c939d8dc23395dfd22ca99462a
linruohan <mjt1220@126.com>
2026-06-13 07:42:46 +0800

    update nvim-config from master

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,7 +8,7 @@ local M = {}
 local highlights = require("highlights")
 
 M.base46 = {
-  theme = "catppuccin",
+  theme = "gruvchad",
   transparency = false,
   hl_override = highlights.override,
   hl_add = highlights.add,
